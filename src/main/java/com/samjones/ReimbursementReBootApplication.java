86056a60c5c5a963777ec4ecbbd3ea76617301a7
Sam <samuelriley1393@gmail.com>
2019-12-16 18:59:35 -0500

    successfully connected to jawsdb

--- src/main/java/com/samjones/ReimbursementReBootApplication.java
+++ src/main/java/com/samjones/ReimbursementReBootApplication.java
@@ -1,5 +1,11 @@
 package com.samjones;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +16,14 @@ public class ReimbursementReBootApplication {
 		SpringApplication.run(ReimbursementReBootApplication.class, args);
 	}
 
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+	    String username = jdbUri.getUserInfo().split(":")[0];
+	    String password = jdbUri.getUserInfo().split(":")[1];
+	    String port = String.valueOf(jdbUri.getPort());
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+
+	    return DriverManager.getConnection(jdbUrl, username, password);
+	}
 }
