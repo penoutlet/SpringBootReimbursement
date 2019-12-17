8b3f968d2e882143db56c7272b0f3706a8beef0e
Sam <samuelriley1393@gmail.com>
2019-12-16 19:59:09 -0500

    Update ReimbursementReBootApplication.java

--- src/main/java/com/samjones/ReimbursementReBootApplication.java
+++ src/main/java/com/samjones/ReimbursementReBootApplication.java
@@ -16,14 +16,5 @@ public class ReimbursementReBootApplication {
 		SpringApplication.run(ReimbursementReBootApplication.class, args);
 	}
 
-	private static Connection getConnection() throws URISyntaxException, SQLException {
-	    URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
 
-	    String username = jdbUri.getUserInfo().split(":")[0];
-	    String password = jdbUri.getUserInfo().split(":")[1];
-	    String port = String.valueOf(jdbUri.getPort());
-	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-
-	    return DriverManager.getConnection(jdbUrl, username, password);
-	}
 }
